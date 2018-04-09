1c7d18296bab0b8594464c9fd19ebb4008989dc2
JensMalm <jensmalm873@gmail.com>
2018-04-09 10:11:14 +0200

    Add project files.

---
+++ SQLCRM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQLCRM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
